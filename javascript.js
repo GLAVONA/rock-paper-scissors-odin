8e2be943f96b0b16046e6c69f50c09c05c62fc35
GLAVONA <97589269+GLAVONA@users.noreply.github.com>
2022-11-13 14:44:12 +0200

    Added UI and fleshed out gameplay.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,38 @@
+const rockButton = document.querySelector(".rock");
+const paperButton = document.querySelector(".paper");
+const scissorsButton = document.querySelector(".scissors");
+
+const resultDiv = document.querySelector(".result");
+
+let playerScore = 0;
+let compScore = 0;
+let roundCount = 0;
+let newGame = false;
+
+
+rockButton.addEventListener("click", function (){
+    playRound("rock",getComputerChoice())
+});
+paperButton.addEventListener("click",function (){
+    playRound("paper",getComputerChoice())
+});
+scissorsButton.addEventListener("click", function (){
+    playRound("scissors",getComputerChoice())
+});
+
+let playerScoreP = document.querySelector(".score .player-score");
+let computerScoreP = document.querySelector(".score .computer-score");
+
+function updateScore(updatePlayer, updateComp){
+    playerScoreP.innerText = `Player: ${updatePlayer}`;
+    computerScoreP.innerText = `Computer: ${updateComp}`;
+}
+
+function resetScore(){
+    playerScore = 0;
+    compScore = 0;
+}
+
 function getComputerChoice(){
     let rand = Math.floor(Math.random()*(3-1+1)+1);
 
@@ -12,80 +47,79 @@ function getComputerChoice(){
 }
 
 function playRound(playerSelection, computerSelection){
+    if (newGame){
+        resultDiv.replaceChildren();
+    }
+    newGame=false;
 
-    let player = playerSelection.toLowerCase();
-    let comp = computerSelection.toLowerCase();
+    let player = String(playerSelection).toLowerCase();
+    let comp = String(computerSelection).toLowerCase();
+    const resultP = document.createElement('p');
+    resultDiv.appendChild(resultP);
 
     if (player==="rock"){
-        if (comp==="rock") {
-            return "It's a draw!";
+        if (comp==="rock") {            
+             resultP.innerText = "It's a draw!";
         }
         if (comp==="paper") {
-            return "You lose! Paper covers rock!";
+             resultP.innerText= "You lose! Paper covers rock!";
         }
         if (comp==="scissors"){
-            return "You win! Rock breaks scissors!";
+             resultP.innerText="You win! Rock breaks scissors!";
         }
     }
 
     else if (player==="paper"){
         if (comp==="rock") {
-            return "You win! Paper covers rock!";
+             resultP.innerText="You win! Paper covers rock!";
+             "You win! Paper covers rock!";
         }
         if (comp==="paper") {
-            return "It's a draw!";
+             resultP.innerText="It's a draw!";
         }
         if (comp==="scissors"){
-            return "You lose! Scissors cut paper!";
+             resultP.innerText="You lose! Scissors cut paper!";
         }
     }
 
     else if (player==="scissors"){
         if (comp==="rock") {
-            return "You lose! Rock breaks scissors!";
+             resultP.innerText="You lose! Rock breaks scissors!";
         }
         if (comp==="paper") {
-            return "You win! Scissors cut paper!";
+             resultP.innerText="You win! Scissors cut paper!";
         }
         if (comp==="scissors"){
-            return "It's a draw!";
+             resultP.innerText="It's a draw!";
+            
         }
     }
     else{
-        return "Please choose between Rock/Paper/Scissors";
+        alert("Please choose between Rock/Paper/Scissors");
     }
+    if (resultDiv.lastChild.innerText.includes("win")){
+        playerScore++;
+    }
+    else if (resultDiv.lastChild.innerText.includes("lose")){
+        compScore++;
+    }
+    updateScore(playerScore,compScore);
+    roundCount++;
+    if (roundCount==5){
+        const endResult = document.createElement('p');
+        resultDiv.appendChild(endResult);
+        if (playerScore>compScore){
+            endResult.innerText="Game over! You won!";
+        }
+        else if (playerScore<compScore){
+            endResult.innerText="Game over! You lost!";
+        }
+        else {
+            endResult.innerText= "The game finished in a draw!";
+        }
+        roundCount=0;
+        newGame=true;
+        resetScore();
+    } 
+
 }
-
-function game(){
-    let playerScore = 0;
-    let compScore = 0;
-
-    for (let i = 0; i < 5; i++) {
-        let result = playRound(prompt("Please make your choice:"),getComputerChoice())
-        console.log(result);
-
-        if (result.includes("win")){
-            playerScore++;
-        }
-        else if (result.includes("lose")){
-            compScore++;
-        }
-        else if (result.includes("draw")){
-            continue;
-        }
-        else{
-            i-=1;
-        }
-    }
-
-    if (playerScore>compScore){
-        console.log("Game over! You won!");
-    }
-    else if (playerScore<compScore){
-        console.log("Game over! You lost!");
-    }
-    else {
-        console.log("The game finished in a draw!")
-    }
-}
-
